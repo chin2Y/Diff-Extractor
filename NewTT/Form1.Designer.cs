d91e3440606c070c73a26ba373e95cdce35061dc
chin2Y <saurabhlawange@gmail.com>
2018-12-23 14:42:16 +0530

    Add files via upload

---
+++ NewTT/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace NewTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 86);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(661, 643);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Exam Paper";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(747, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start Exam";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.richTextBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(679, 86);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ShortcutsEnabled = false;
+            this.richTextBox2.Size = new System.Drawing.Size(665, 643);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ NewTT/Form1.cs
@@ -0,0 +1,57 @@
+﻿using DiffMatchPatch;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NewTT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dmp = new diff_match_patch();
+            string text1 = richTextBox1.Text;
+            string text2 = richTextBox2.Text;
+            var diff = dmp.diff_main(text1, text2);
+            dmp.patch_make(text1, text2);
+            
+            richTextBox2.Text = "";
+            foreach (var d in diff)
+            {
+                int length = richTextBox2.Text.Length;
+                if (d.operation == Operation.INSERT)
+                {
+                    Console.WriteLine("+ : " + d.text);
+                    richTextBox2.Text += d.text;
+                    richTextBox2.Select(length, d.text.Length);
+                    richTextBox2.SelectionBackColor = Color.Blue;                    
+                }
+                else
+                {
+                    if (d.operation == Operation.DELETE)
+                    {
+                        Console.WriteLine("- : " + d.text);
+                        richTextBox2.Text += d.text;
+                        richTextBox2.Select(length, d.text.Length);
+                        richTextBox2.SelectionBackColor = Color.Red;
+                    }
+                    else
+                    {
+                        Console.WriteLine("= : " + d.text);
+                        richTextBox2.Text += d.text;
+                    }
+                }
+            }
+        }
+    }    
+}
